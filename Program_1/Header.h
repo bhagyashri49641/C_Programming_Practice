1be5cb25ddfda966d364f94318382a6e808ce2d2
Bhagyashri Mane <53085622+bhagyashri49641@users.noreply.github.com>
2020-07-30 20:37:29 +0530

    Delete Header.h

--- Program_1/Header.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>                       // Used for IO functions
-
-typedef int BOOLEAN;                // New datatype as BOOLEAN treated as int
-
-#define TRUE 1                      // User defined macro
-#define FALSE 0                     // User defined macro
-
-BOOLEAN ChkEvenOdd(int);    // Function prototype
